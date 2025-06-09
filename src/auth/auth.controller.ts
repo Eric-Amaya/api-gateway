daa8944f9176fd19e1ed211ad9d16448e4ec34be
FranciscoLopezKanto <91705663+FranciscoLopezKanto@users.noreply.github.com>
2025-06-09 16:58:38 -0400

    Update document

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,10 @@ import { Roles } from './decorators/role.decorator';
 import { RolesGuard } from './guards/roles.guard';
 import { Request, Response } from 'express';
 import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
+import { UpdateDocumentDto } from './dto/update-document.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { Role } from './enum/role.enum';
-import { UpdateDocumentDto } from './dto/update-document.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -96,13 +96,13 @@ export class AuthController {
     }
 
     @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ADMIN)
+    @Roles(Role.ADMIN ,Role.ADMIN2)
     @Get('user/:_id')
     getUserById(@Param('_id') _id: string): Promise<UserRequestDto | null> {
         return this.authService.getUserById(_id);
     }
 
-    @UseGuards(JwtAuthGuard)
+    //@UseGuards(JwtAuthGuard)
     @Get('users')
     getUsers(): Promise<UserRequestDto[]> {
         return this.authService.getAllUsers();
