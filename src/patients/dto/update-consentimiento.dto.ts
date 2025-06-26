b03b95aa09ee0602635690d5ae2833a6bf7c640c
FranciscoLopezKanto <91705663+FranciscoLopezKanto@users.noreply.github.com>
2025-06-26 10:21:53 -0400

    firma consentimiento extra en update

--- src/patients/dto/update-consentimiento.dto.ts
+++ src/patients/dto/update-consentimiento.dto.ts
@@ -5,6 +5,10 @@ export class UpdateConsentimientoDto {
   @IsDateString()
   fechaFirma?: string;
 
+   @IsOptional()
+  @IsDateString()
+  fechaImplementacion?: string;
+
   @IsString()
   version: string;
 
